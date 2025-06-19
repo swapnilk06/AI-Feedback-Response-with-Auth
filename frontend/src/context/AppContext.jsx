1755ac5f614d445100587fdea18129dca5da393f
Swapnil Kathale <swapnilkathale111@gmail.com>
2025-06-19 11:20:57 +0530

    update: AppContext code to solve issue

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,11 +1,10 @@
 import React, { useState, useContext, useEffect, createContext } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
-
 export const AppContent = createContext();
 
-// send the cookies
 export const AppContextProvider = (props) => {
+  // send the cookies
   axios.defaults.withCredentials = true;
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
@@ -15,28 +14,24 @@ export const AppContextProvider = (props) => {
 
   const getAuthState = async () => {
     try {
-      const { data } = await axios.get(`${backendUrl}/api/auth/is-auth`);
+      const { data } = await axios.get(backendUrl + "/api/auth/is-auth", {
+        withCredentials: true,
+      });
+
       if (data.success) {
         setIsLoggedin(true);
         localStorage.setItem("auth", "true"); // Save flag for refresh
         await getUserData();
-      } else {
-        setIsLoggedin(false);
-        setUserData(false);
-        localStorage.removeItem("auth");
       }
     } catch (error) {
       setIsLoggedin(false);
-      setUserData(false);
       localStorage.removeItem("auth");
-    } finally {
-      setLoadingAuthCheck(false);
     }
   };
 
   const getUserData = async () => {
     try {
-      const { data } = await axios.get(`${backendUrl}/api/user/data`, {
+      const { data } = await axios.get(backendUrl + "/api/user/data", {
         withCredentials: true,
       });
 
@@ -48,24 +43,21 @@ export const AppContextProvider = (props) => {
           toast.error(data.message || "User not found");
         }
         setIsLoggedin(false);
-        setUserData(false);
         localStorage.removeItem("auth");
       }
     } catch (error) {
       const alreadyLoggedIn = localStorage.getItem("auth");
       if (alreadyLoggedIn) {
-        toast.error(error.response?.data?.message || "Failed to get user data");
+        toast.error(error.response?.data?.message || "User not found");
+        localStorage.removeItem("auth");
       }
       setIsLoggedin(false);
-      setUserData(false);
-      localStorage.removeItem("auth");
     }
   };
 
   useEffect(() => {
     getAuthState();
   }, []);
-
   const value = {
     backendUrl,
     isLoggedin,
